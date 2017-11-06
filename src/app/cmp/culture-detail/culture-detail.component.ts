a372486816fe548202f5f0b84cee6a7f5540dc70
joshuaadamyoung <joshuaadamyoung@gmail.com>
2017-11-06 14:34:11 -0600

    doing stuff

--- src/app/cmp/culture-detail/culture-detail.component.ts
+++ src/app/cmp/culture-detail/culture-detail.component.ts
@@ -7,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CultureDetailComponent implements OnInit {
   reviews: object[];
+  data: object;
   constructor() {
+    this.data = {
+      image: 'This is an image placeholder',
+      id: 12345,
+      name: 'Scobtaculus',
+      desc: 'This scoby will leave you with a vey fizzy very delicious kombucha. Tends towoards slight tanginess compared to other scoby\'s. Has been fed cocunut sugar almost exclusively.',
+      family: 'SCOBY',
+      type: 'Kombucha',
+      environment: {
+        bioregion: 'cascadia',
+        subregion: 'rural',
+        localEnvironment: 'forest'
+      },
+      parentCulture: 11342,
+      childCultures: [
+        16623,
+        79251,
+        92451
+      ]
+    };
     this.reviews = [
       {
         user: 'SomeGuy',
