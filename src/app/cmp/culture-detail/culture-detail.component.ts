899e269b1f1b98e1213d54b4fae324b579507826
joshuaadamyoung <joshuaadamyoung@gmail.com>
2017-11-06 12:42:47 -0600

    working on culture detail a bit

--- src/app/cmp/culture-detail/culture-detail.component.ts
+++ src/app/cmp/culture-detail/culture-detail.component.ts
@@ -6,8 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./culture-detail.component.css']
 })
 export class CultureDetailComponent implements OnInit {
-
-  constructor() { }
+  reviews: object[];
+  constructor() {
+    this.reviews = [
+      {
+        user: 'SomeGuy',
+        review: 'Wow, what a scoby! The booch tastes great!',
+        date: new Date(2017, 11, 5),
+        rating: 7.2
+      },
+      {
+        user: 'SomeGuy',
+        review: 'Wow, what a scoby! The booch tastes great!',
+        date: new Date(2017, 11, 5),
+        rating: 7.2
+      },
+      {
+        user: 'SomeGuy',
+        review: 'Wow, what a scoby! The booch tastes great!',
+        date: new Date(2017, 11, 5),
+        rating: 7.2
+      },
+      {
+        user: 'SomeGuy',
+        review: 'Wow, what a scoby! The booch tastes great!',
+        date: new Date(2017, 11, 5),
+        rating: 7.2
+      }
+    ]
+  }
 
   ngOnInit() {
   }
